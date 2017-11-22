78617590ce4f2b35d48e3721e17b9cfe544d0e9b
guillaume-gomez <guillaume.gomez846@gmail.com>
2017-11-22 13:57:40 +0100

    rename files

---
+++ recognition.js
@@ -0,0 +1,43 @@
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+if (annyang) {
+  // Let's define our first command. First the text we expect, and then the function it should call
+  // action is a variable and will get passed to the calling function
+  var commands = {
+    'music :action': function(action) {
+      console.log(action);
+    }
+  };
+
+  // Add our commands to annyang
+  annyang.addCommands(commands);
+
+  // Start listening. You can call this here, or attach this call to an event, button, etc.
+  annyang.start();
+}
+
+recognition.onresult = (event) => {
+  // event is a SpeechRecognitionEvent and it holds all the lines captured so far
+
+  // get the current line
+  var current = event.resultIndex;
+
+  // get the recognized text
+  var transcript = event.results[current][0].transcript;
+
+  console.log(transcript);
+}
+
+recognition.onstart = () => {
+  console.log("voice recognition activated");
+}
+
+recognition.onend = () => {
+  console.log("voice recognition ended");
+  recognition.start();
+}
+
+recognition.onerror = (event) => {
+  console.error(event);
+}
