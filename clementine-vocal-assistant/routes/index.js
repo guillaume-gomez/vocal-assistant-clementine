0ffb747bb90cbb834699268d39ec045aa580ff8d
guillaume-gomez <guillaume.gomez846@gmail.com>
2017-11-24 14:03:05 +0100

    detect which action is called by the client

--- clementine-vocal-assistant/routes/index.js
+++ clementine-vocal-assistant/routes/index.js
@@ -6,4 +6,70 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/music/:action', (req, res) => {
+  const options = parseOptions(req.params.action.toLowerCase());
+  console.log(options)
+  // const command = spawn('clementine', options, {
+  //   detached: true,
+  //   stdio: 'ignore'
+  // });
+
+  // command.on('close', (code, signal) => {
+  //   const optionsStr = options && options.length ? options.join(' ') : ''
+  //   this
+  //     .logger
+  //     .info(`finished executing command to ${this.bin} with ${optionsStr}`);
+  // });
+
+  // command.unref();
+
+  res.sendStatus(200);
+});
+
+const parseOptions = (action) => {
+  let options = [];
+  let flag = '';
+
+  switch (action) {
+    case 'play':
+      flag = '-p';
+      break;
+    case 'stop':
+      flag = '-s';
+      break;
+    case 'pause':
+      flag = '-u';
+      break;
+    case 'volume-up':
+      flag = '--volume-up';
+      break;
+    case 'volume-down':
+      flag = '--volume-down';
+      break;
+    case 'next':
+      flag = '-f';
+      break;
+    case 'previous':
+      flag = '-r';
+      break;
+  }
+
+  if (action.startsWith('set-volume')) {
+    const split = action.split('-');
+    flag = '-v ' + split[2];
+  }
+
+  if (action.startsWith('seek-by')) {
+    const split = action.split('-');
+    options = ['--seek-by', split[2]];
+  }
+
+  if (options.length) {
+    return options;
+  }
+
+  return [flag];
+}
+
+
 module.exports = router;
