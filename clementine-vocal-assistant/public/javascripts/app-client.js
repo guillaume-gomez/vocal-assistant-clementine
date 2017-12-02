6850f7396bef19c43d103d6b806cd68a11347156
guillaume-gomez <guillaume.gomez846@gmail.com>
2017-12-03 00:45:41 +0100

    create react entry point

--- clementine-vocal-assistant/public/javascripts/app-client.js
+++ clementine-vocal-assistant/public/javascripts/app-client.js
@@ -1,1 +1,35 @@
-app-clients.js
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    [].forEach(item => {
+      this[item] = this[item].bind(this);
+    });
+    this.state = {};
+  }
+
+  componentDidMount() {
+    console.log("hjsd")
+  }
+
+  getParams(data) {
+    //remove ?pages=
+    return data.substring(6);
+  }
+
+  updateQueryString(params) {
+    if (window.history.pushState) {
+      var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + `?page=${params}`;
+      window.history.pushState({path:newurl},'',newurl);
+    }
+  }
+
+  render () {
+    return (<h1>"Hi i"m a react component"</h1>);
+  }
+}
+console.log("binouzes");
+render(<App/>, document.getElementById('app'));
